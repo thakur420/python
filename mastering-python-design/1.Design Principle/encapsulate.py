45eb59f006bb6d753add87d9aa2056951a895d39
Indrajeet Thakur <38977906+thakur420@users.noreply.github.com>
2024-06-08 11:53:51 +0530

    design principle

---
+++ mastering-python-design/1.Design Principle/encapsulate.py
@@ -0,0 +1,20 @@
+class PaymentBase:
+    def __init__(self, amount : int) -> None:
+        self.amount = amount
+    def process_payment(self):
+        pass
+
+class CreditCard(PaymentBase):
+    def process_payment(self):
+        msg = f"Credit card payment: {self.amount}"
+        print(msg)
+
+class PayPal(PaymentBase):
+    def process_payment(self):
+        msg = f"PayPal paymet: {self.amount}"
+        print(msg)
+
+if __name__ == "__main__":
+    payments = [CreditCard(100),PayPal(200)]
+    for payment in payments:
+        payment.process_payment()
